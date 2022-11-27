1b8e26646b23bd3a8761594271a31840516de487
fred <daktarispacetime@gmail.com>
2022-11-27 15:04:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+
+
+function saturdayFun(activity="roller-skate"){
+return 'This Saturday, I want to ' + activity + '!';
+ }
+saturdayFun("bathe my dog");
+ 
+ 
+function mondayWork(activity="go to the office"){
+return 'This Monday, I will ' + activity + '.';
+}
+ 
+mondayWork("start coding");
+ 
+function wrapAdjective(flair="*"){
+return function(text="special"){
+return `You are ${flair}${text}${flair}!`
+}
+}
